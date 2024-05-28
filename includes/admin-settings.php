7920aaa341da00d51fb20ed586224a8906a14428
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-05-28 13:35:14 +0200

    Update admin-settings.php

--- includes/admin-settings.php
+++ includes/admin-settings.php
@@ -1,50 +1,48 @@
 <?php
 
-// Prevent direct access
-if (!defined('ABSPATH')) {
-    exit;
+function pi_sma_register_settings() {
+    add_option('pi_sma_email_server_incoming', '');
+    add_option('pi_sma_email_server_outgoing', '');
+    add_option('pi_sma_email_username', '');
+    add_option('pi_sma_email_password', '');
+    register_setting('pi_sma_options_group', 'pi_sma_email_server_incoming', 'sanitize_text_field');
+    register_setting('pi_sma_options_group', 'pi_sma_email_server_outgoing', 'sanitize_text_field');
+    register_setting('pi_sma_options_group', 'pi_sma_email_username', 'sanitize_text_field');
+    register_setting('pi_sma_options_group', 'pi_sma_email_password', 'sanitize_text_field');
 }
+add_action('admin_init', 'pi_sma_register_settings');
 
-function sma_admin_menu() {
-    add_menu_page(
-        'SmartMail Assistant Settings',
-        'SmartMail Assistant',
-        'manage_options',
-        'sma-settings',
-        'sma_settings_page',
-        'dashicons-email',
-        100
-    );
+function pi_sma_register_options_page() {
+    add_options_page('SmartMail Pi Settings', 'SmartMail Pi', 'manage_options', 'pi_sma', 'pi_sma_options_page');
 }
-add_action('admin_menu', 'sma_admin_menu');
+add_action('admin_menu', 'pi_sma_register_options_page');
 
-function sma_settings_page() {
-    ?>
-    <div class="wrap">
-        <h1>SmartMail Assistant Settings</h1>
-        <form method="post" action="options.php">
-            <?php
-            settings_fields('sma_settings_group');
-            do_settings_sections('sma-settings');
-            submit_button();
-            ?>
-        </form>
-    </div>
-    <?php
-}
-
-function sma_register_settings() {
-    register_setting('sma_settings_group', 'sma_api_key', 'sanitize_text_field');
-    add_settings_section('sma_main_section', 'Main Settings', 'sma_main_section_callback', 'sma-settings');
-    add_settings_field('sma_api_key', 'API Key', 'sma_api_key_callback', 'sma-settings', 'sma_main_section');
-}
-add_action('admin_init', 'sma_register_settings');
-
-function sma_main_section_callback() {
-    echo '<p>Enter your API key to enable the SmartMail Assistant features.</p>';
-}
-
-function sma_api_key_callback() {
-    $api_key = get_option('sma_api_key');
-    echo '<input type="text" name="sma_api_key" value="' . esc_attr($api_key) . '" class="regular-text" />';
+function pi_sma_options_page() {
+?>
+  <div>
+  <h2>SmartMail Pi Settings</h2>
+  <form method="post" action="options.php">
+  <?php settings_fields('pi_sma_options_group'); ?>
+  <table>
+  <tr valign="top">
+  <th scope="row"><label for="pi_sma_email_server_incoming">Incoming Mail Server</label></th>
+  <td><input type="text" id="pi_sma_email_server_incoming" name="pi_sma_email_server_incoming" value="<?php echo esc_attr(get_option('pi_sma_email_server_incoming')); ?>" /></td>
+  </tr>
+  <tr valign="top">
+  <th scope="row"><label for="pi_sma_email_server_outgoing">Outgoing Mail Server</label></th>
+  <td><input type="text" id="pi_sma_email_server_outgoing" name="pi_sma_email_server_outgoing" value="<?php echo esc_attr(get_option('pi_sma_email_server_outgoing')); ?>" /></td>
+  </tr>
+  <tr valign="top">
+  <th scope="row"><label for="pi_sma_email_username">Email Username</label></th>
+  <td><input type="text" id="pi_sma_email_username" name="pi_sma_email_username" value="<?php echo esc_attr(get_option('pi_sma_email_username')); ?>" /></td>
+  </tr>
+  <tr valign="top">
+  <th scope="row"><label for="pi_sma_email_password">Email Password</label></th>
+  <td><input type="password" id="pi_sma_email_password" name="pi_sma_email_password" value="<?php echo esc_attr(get_option('pi_sma_email_password')); ?>" /></td>
+  </tr>
+  </table>
+  <?php submit_button(); ?>
+  </form>
+  </div>
+<?php
 }
