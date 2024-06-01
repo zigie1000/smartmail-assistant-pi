cc9ecd2c339c220463cddd93d01ee8dc087ce5cd
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-01 15:52:57 +0200

    Update admin-settings.php

--- includes/admin-settings.php
+++ includes/admin-settings.php
@@ -1,48 +1,17 @@
 <?php
+// Admin settings for SmartMail Assistant Pi Plugin
 
-function pi_sma_register_settings() {
-    add_option('pi_sma_email_server_incoming', '');
-    add_option('pi_sma_email_server_outgoing', '');
-    add_option('pi_sma_email_username', '');
-    add_option('pi_sma_email_password', '');
-    register_setting('pi_sma_options_group', 'pi_sma_email_server_incoming', 'sanitize_text_field');
-    register_setting('pi_sma_options_group', 'pi_sma_email_server_outgoing', 'sanitize_text_field');
-    register_setting('pi_sma_options_group', 'pi_sma_email_username', 'sanitize_text_field');
-    register_setting('pi_sma_options_group', 'pi_sma_email_password', 'sanitize_text_field');
+// Register settings
+function smartmail_pi_register_settings() {
+    register_setting('smartmail_pi_settings', 'smartmail_pi_api_key');
+    add_settings_section('smartmail_pi_section', 'API Settings', null, 'smartmail-pi');
+    add_settings_field('smartmail_pi_api_key', 'API Key', 'smartmail_pi_api_key_callback', 'smartmail-pi', 'smartmail_pi_section');
 }
-add_action('admin_init', 'pi_sma_register_settings');
+add_action('admin_init', 'smartmail_pi_register_settings');
 
-function pi_sma_register_options_page() {
-    add_options_page('SmartMail Pi Settings', 'SmartMail Pi', 'manage_options', 'pi_sma', 'pi_sma_options_page');
+// API Key field callback
+function smartmail_pi_api_key_callback() {
+    $api_key = get_option('smartmail_pi_api_key');
+    echo '<input type="text" name="smartmail_pi_api_key" value="' . esc_attr($api_key) . '" class="regular-text">';
 }
-add_action('admin_menu', 'pi_sma_register_options_page');
-
-function pi_sma_options_page() {
 ?>
-  <div>
-  <h2>SmartMail Pi Settings</h2>
-  <form method="post" action="options.php">
-  <?php settings_fields('pi_sma_options_group'); ?>
-  <table>
-  <tr valign="top">
-  <th scope="row"><label for="pi_sma_email_server_incoming">Incoming Mail Server</label></th>
-  <td><input type="text" id="pi_sma_email_server_incoming" name="pi_sma_email_server_incoming" value="<?php echo esc_attr(get_option('pi_sma_email_server_incoming')); ?>" /></td>
-  </tr>
-  <tr valign="top">
-  <th scope="row"><label for="pi_sma_email_server_outgoing">Outgoing Mail Server</label></th>
-  <td><input type="text" id="pi_sma_email_server_outgoing" name="pi_sma_email_server_outgoing" value="<?php echo esc_attr(get_option('pi_sma_email_server_outgoing')); ?>" /></td>
-  </tr>
-  <tr valign="top">
-  <th scope="row"><label for="pi_sma_email_username">Email Username</label></th>
-  <td><input type="text" id="pi_sma_email_username" name="pi_sma_email_username" value="<?php echo esc_attr(get_option('pi_sma_email_username')); ?>" /></td>
-  </tr>
-  <tr valign="top">
-  <th scope="row"><label for="pi_sma_email_password">Email Password</label></th>
-  <td><input type="password" id="pi_sma_email_password" name="pi_sma_email_password" value="<?php echo esc_attr(get_option('pi_sma_email_password')); ?>" /></td>
-  </tr>
-  </table>
-  <?php submit_button(); ?>
-  </form>
-  </div>
-<?php
-}
